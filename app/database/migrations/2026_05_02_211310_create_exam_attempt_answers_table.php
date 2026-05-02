25280c11405fa6f84757fcb93cc06f363098525c
Dêvisson Souza <deivisson.souza@satcompany.com.br>
2026-05-02 18:18:06 -0300

    feat: criação da estrutura inicial do domínio de provas

---
+++ app/database/migrations/2026_05_02_211310_create_exam_attempt_answers_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+  /**
+   * Run the migrations.
+   */
+  public function up(): void
+  {
+    Schema::create('exam_attempt_answers', function (Blueprint $table) {
+      $table->id();
+
+      $table->foreignId('exam_attempt_id')
+        ->constrained()
+        ->cascadeOnDelete();
+
+      $table->foreignId('question_id')
+        ->constrained()
+        ->cascadeOnDelete();
+
+      $table->foreignId('alternative_id')
+        ->constrained()
+        ->cascadeOnDelete();
+
+      $table->boolean('is_correct')->default(false);
+
+      $table->timestamps();
+
+      $table->unique(['exam_attempt_id', 'question_id']);
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   */
+  public function down(): void
+  {
+    Schema::dropIfExists('exam_attempt_answers');
+  }
+};
